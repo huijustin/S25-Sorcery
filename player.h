13e537331e56f025d2cb7e0187d723920b5584b1
Justin Hui <j49hui@uwaterloo.ca>
2025-07-20 17:14:24 -0400

    updated player.h

--- player.h
+++ player.h
@@ -2,9 +2,29 @@
 #define PLAYER_H
 #include "deck.h"
 #include "hand.h"
+#include <string>
 
 class Player {
+    std::string name;
+    int life;
+    int magic;
+    Deck deck;
+    Hand hand;
+  public:
+    Player(const std::string &name);
 
+    // draw a card from deck and add it to hand
+    void drawCard();
+    void startTurn();
+    void endTurn();
+
+    // accessors / setters
+    const std::string getName() const;
+    int getLife() const;
+    void setLife(int life);
+    int getMagic() const;
+    void setMagic(int magic);
+    
 }
 
 #endif
