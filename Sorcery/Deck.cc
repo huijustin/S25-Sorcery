912875957ff9bd37f2f9a572f3c95941958fc082
Vishtheman <vishwavk2005@gmail.com>
2025-07-20 21:51:28 -0400

    Create Deck.cc
    Created Deck.cc and uploaded implementation for load_deck

---
+++ Sorcery/Deck.cc
@@ -0,0 +1,21 @@
+#include "Deck.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+void  GameEngine::load_deck(const std::string& filename) {
+    std::ifstream file(filename);
+    std::string line;
+
+    if (!(file.is_open())) {
+        std::cerr << "Could not open deck file: " << filename << std::endl; // Check Error message
+        return;
+    }
+
+    while (std::getline(file, line)) {
+
+        std::cout << "loaded card: " << line << std::endl; // Check message
+    }
+
+    file.close();
+}
