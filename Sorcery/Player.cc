48ace3e1b10b583b91ef9e55e9164e1e8a34a6ec
Justin Hui <j49hui@uwaterloo.ca>
2025-07-21 22:10:56 -0400

    Added Player class interface and implementations

---
+++ Sorcery/Player.cc
@@ -0,0 +1,71 @@
+#include "Player.h"
+#include <iostream>
+
+Player::Player(const std::string &name, const std::string& deckFile) :
+  name{name}, life{20}, magic{3}, ritual{nullptr} {
+    deck.load_deck(deckFile);
+    deck.shuffle();
+    for (int i = 0; i < 5; ++i) {
+        drawCard();
+    }
+}
+
+std::string Player::getName() const { return name; }
+int Player::getLife() const { return life; }
+int getMagic() const { return magic; }
+void setLife(int l) { life = l; }
+void setMagic(int m) { magic = m; }
+
+void Player::takeDamage(int amount) { life -= amount; }
+void Player::gainMagic(int amount) { magic += amount; }
+void Player::spendMagic(int cost) { mgaic -= cost; }
+
+
+void Player::startTurn() {
+    gainMagic(1);
+    drawCard();
+    // TODO: trigger "start of turn" effects (rituals, triggers)
+}
+
+void Player::PlayCard(int idx) {
+    Card* c = hand.removeCard(idx);
+    if (!c) {
+        std::cerr << "Error: cannot play card" << std::endl;
+        return;
+    }
+    spendMagic(c->getCost());
+    c->play();
+    // TODO: place c on board, graveyard, or ritual slot based on dynamic type
+}
+
+void Player::attack(int fromIdx, int toIdx) {
+    if (fromIdx < 1 || fromIdx > board.size()) {
+        std::cerr << "Error: Invalid attacker Index" << std::endl;
+        return;
+    }
+    Minion* attacker = board.getMinions()[fromIdx - 1];
+    if (toIdx == 0) {
+        // attack opposing player
+        // TODO: implement in GameEngine context
+    } else {
+        // attack opposing minon
+        // TODO: look up opposing player's board and resolve combat
+    }
+}
+
+void Player::useAbility(int fromIdx, int targetIdx) {
+    if (fromIdx < 1 || fromIdx > board.size()) {
+        std::cerr << "Error: Invalid attacking minion index" << std::endl;
+        return;
+    }
+    Minion* m = board.getMinions()[fromIdx - 1];
+    // Todo: implement m->activateAbility(target)
+}
+
+void Player::drawCard() {
+    if (!hand.isFull() && !deck.isEmpty()) {
+        Card* c = deck.draw();
+        hand.addCard(c);
+    }
+}
+
--- Sorcery/Player.h
+++ Sorcery/Player.h
@@ -1,10 +1,13 @@
-#pragma once
+#ifndef PLAYER_H
+#define PLAYER_H
 #include <string>
 #include "Deck.h"
-#include "Board.h"
 #include "Hand.h"
+#include "Board.h"
 #include "Graveyard.h"
 #include "Ritual.h"
+#include "Minion.h"
+#include "Card.h"
 
 class Player {
     std::string name;
@@ -14,11 +17,30 @@ class Player {
     Hand hand;
     Board board;
     Graveyard graveyard;
-    Ritual ritual;
+    Ritual* ritual;
 
 public:
-    int getLife() const;
-    void playCard(int index);
+    // ctor
+    Player(const std::string &name, Deck initDeck);
+
+    // public methods
+    void startTurn();
+    void playCard(int idx);
     void attack(int fromIdx, int toIdx);
-    void useAbility(int fromIdx, int toIdx);
+    void useAbility(int fromIdx, int targetIdx);
+    void drawCard();
+
+    // accessors / setters
+    std::string getName() const;
+    int getLife() const;
+    int getMagic() const;
+    void setLife(int l);
+    void setMagic(int m);
+
+    // game actions
+    void takeDamage(int amount);
+    void gainMagic(int amount);
+    void spendMagic(int cost);
 };
+
+#endif
