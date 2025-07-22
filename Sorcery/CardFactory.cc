f09b957ec874f76278baf3f75aa290568ecb6347
cnphan <cnphan@uwaterloo.ca>
2025-07-22 16:22:31 -0400

    Fixed minionIDs

--- Sorcery/CardFactory.cc
+++ Sorcery/CardFactory.cc
@@ -8,13 +8,13 @@ CardFactory::CardFactory() {
 
     // Minions                 CardID, Name,        Cost,ATK,Def,Ability,                  Card Text
     masterList.push_back(new Minion(0, "Air Elemental", 0, 1, 1, nullptr, ""));
-    masterList.push_back(new Minion(0, "Earth Elemental", 3, 4, 4, nullptr, ""));
-    masterList.push_back(new Minion(0, "Bone Golem", 2, 1, 3, nullptr, "Gain +1/+1 whenever a minion leaves play"));
-    masterList.push_back(new Minion(0, "Fire Elemental", 2, 2, 2, nullptr, "Whenever an opponent's minion enters play, deal 1 damage to it"));
-    masterList.push_back(new Minion(0, "Potion Seller", 2, 1, 3, nullptr, "At the end of your turn, all your minions gain +0/+1"));
-    masterList.push_back(new Minion(0, "Novice Pyromancer", 1, 0, 1, nullptr, "Deal 1 damage to target minion"));
-    masterList.push_back(new Minion(0, "Apprentice Summoner", 1, 1, 1, nullptr, "Summon a 1/1 air elemental"));
-    masterList.push_back(new Minion(0, "Master Summoner", 3, 2, 3, nullptr, "Summon up to three 1/1 air elementals"));
+    masterList.push_back(new Minion(1, "Earth Elemental", 3, 4, 4, nullptr, ""));
+    masterList.push_back(new Minion(2, "Bone Golem", 2, 1, 3, nullptr, "Gain +1/+1 whenever a minion leaves play"));
+    masterList.push_back(new Minion(3, "Fire Elemental", 2, 2, 2, nullptr, "Whenever an opponent's minion enters play, deal 1 damage to it"));
+    masterList.push_back(new Minion(4, "Potion Seller", 2, 1, 3, nullptr, "At the end of your turn, all your minions gain +0/+1"));
+    masterList.push_back(new Minion(5, "Novice Pyromancer", 1, 0, 1, nullptr, "Deal 1 damage to target minion"));
+    masterList.push_back(new Minion(6, "Apprentice Summoner", 1, 1, 1, nullptr, "Summon a 1/1 air elemental"));
+    masterList.push_back(new Minion(7, "Master Summoner", 3, 2, 3, nullptr, "Summon up to three 1/1 air elementals"));
 
     // Spells
 
