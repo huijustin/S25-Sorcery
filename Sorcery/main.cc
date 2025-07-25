ff2162c4bbad6f0766dd3b28351d3e737102950b
Vishtheman <vishwavk2005@gmail.com>
2025-07-25 09:45:47 -0400

    Updated main

--- Sorcery/main.cc
+++ Sorcery/main.cc
@@ -1,7 +1,30 @@
 #include "GameEngine.h"
+#include <string>
+#include <iostream>
 
-int main() {
-    GameEngine engine{false, false};
+int main(int argc, char* argv[]) {
+    bool testing = false;
+    bool graphics = false;
+    std::string initFile = "";
+
+    for (int i = 1; i < argc; i++) {
+        std::string arg = argv[i];
+
+        if (arg == "-init" && i + 1 < argc) {
+            initFile = argv[++i];
+        }
+        else if (arg == "-testing") {
+            testing = true;
+        }
+        else if (arg == "-graphics") {
+            graphics = true;
+        }
+        else {
+            std::cerr << "Unkown argument: " << arg << std::endl;
+        }
+    }
+
+    GameEngine engine{testing, graphics, initFile};
     engine.run();
     return 0;
 }
